29795ba3383532663b7988e6e5c57436edbb902d
meldrick_wee <meldrick_wee_gh@aiap.sg>
2023-04-20 09:49:02 +0800

    Modularise langchain function

---
+++ src/query_processor.py
@@ -0,0 +1,51 @@
+import os
+
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.vectorstores import FAISS
+
+
+# TODO: Add more context to the question template
+# TODO: Type hinting and docstrings
+def process_query(api_key, question, path):
+    if os.path.exists(path):
+        vector_store = FAISS.load_local(
+            path, OpenAIEmbeddings(openai_api_key=api_key)
+        )
+    else:
+        return "Upload index.faiss and index.pkl files to {path} directory first"
+
+    system_template = """Use the following pieces of context to answer the users question.\
+    No matter what the question is, you should always answer it in the context of the AI Practitioner Handbook.\
+    Even if the question does not end in a question mark, you should still answer it as if it were a question.\
+    If you don't know the answer, just say that "I don't know", don't try to make up an answer.\
+    ----------------
+    {summaries}"""
+
+    messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+
+    chain_type_kwargs = {"prompt": prompt}
+
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo", temperature=0, openai_api_key=api_key)
+
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs=chain_type_kwargs,
+    )
+
+    result = chain(question)
+    return result
